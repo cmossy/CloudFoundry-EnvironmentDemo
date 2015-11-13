9b2d3a27dc5b815149ca203bdc708bd2160c6f6a
cmossy <clare.morrissey@emc.com>
2015-11-13 10:45:57 +0000

    Update Application.java

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
  * Used to initialize the app when packaged as a JAR
  * @author kkrueger
  */
-public class Application {
+public class Applications {
 
     public static void main(String[] args) {
         SpringApplication.run(Config.class, args);
